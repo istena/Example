9038c29da654180ac77ca6dedd7dfc65edcaf9d9
Ilya <sannikov444@yandex.ru>
2022-11-20 14:13:59 +0300

    added if else

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя ");
+string username=Console.ReadLine();
+if (username.ToLower()=="маша")
+{
+    Console.WriteLine("УРА МАША");
+}
+else {
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
